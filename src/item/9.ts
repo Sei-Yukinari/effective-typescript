bdb13194af219abafa4b9e8b7c6ea7169fb813ba
Sei Yukinari <sei.yukinari@nextremer.com>
2025-06-13 15:04:05 +0900

    chore: clean up and refactor code in 8.ts and add new file 9.ts

---
+++ src/item/9.ts
@@ -0,0 +1,27 @@
+// interface Person {
+//   name: string;
+// }
+// const alice: Person = { name: 'Alice' };
+// const bob = { name: 'Bob' } as Person;
+//
+// const alice2: Person = {};
+// const bob2 = {} as Person;
+//
+// bob2.name;
+//
+// const alice3: Person = {
+//   name: 'Alice',
+//   occupation: 'TypeScript developer',
+// };
+// const bob3 = {
+//   name: 'Bob',
+//   occupation: 'TypeScript developer',
+// } as Person;
+//
+// const people = ['alice', 'bob', 'jan'].map(name => ({ name }));
+//
+// const people2 = ['alice', 'bob', 'jan'].map(name => ({}) as Person);
+//
+// const people3 = ['alice', 'bob', 'jan'].map((name): Person => ({ name }));
+//
+// const people4: Person[] = ['alice', 'bob', 'jan'].map(name => ({ name }));
