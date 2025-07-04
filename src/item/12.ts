52ee76d041cf71305a94f998f557340d7e5a62c9
Sei Yukinari <sei.yukinari@nextremer.com>
2025-07-04 13:04:23 +0900

    feat: add 3D vector interface and component retrieval function in new file 20.ts

--- src/item/12.ts
+++ src/item/12.ts
@@ -1,55 +1,55 @@
-function rollDice1(sides: number): number {
-  return Math.floor(Math.random() * sides) + 1;
-}
-
-const rollDice2 = function (sides: number): number {
-  return Math.floor(Math.random() * sides) + 1;
-};
-
-const rollDice3 = (sides: number) => {
-  return Math.floor(Math.random() * sides) + 1;
-};
-
-function add(a: number, b: number) {
-  return a + b;
-}
-function sub(a: number, b: number) {
-  return a - b;
-}
-function mul(a: number, b: number) {
-  return a * b;
-}
-function div(a: number, b: number) {
-  return a / b;
-}
-
-type BinaryFn = (a: number, b: number) => number;
-const add2: BinaryFn = (a, b) => a + b;
-const sub2: BinaryFn = (a, b) => a - b;
-const mul2: BinaryFn = (a, b) => a * b;
-const div2: BinaryFn = (a, b) => a / b;
-
-async function getQuote() {
-  const response = await fetch('/quote?by=Mark+Twain');
-  return await response.json();
-}
-
-// async function checkedFetch(input: RequestInfo, init?: RequestInit) {
-//   const response = await fetch(input, init);
-//   if (!response.ok) {
-//     // async関数で例外を投げると拒否されたPromiseになる
-//     throw new Error(`Request failed: ${response.status}`);
-//     return response;
-//   }
+// function rollDice1(sides: number): number {
+//   return Math.floor(Math.random() * sides) + 1;
 // }
-
-// async function checkedFetch(
-//   input: RequestInfo,
-//   init?: RequestInit
-// ): Promise<Response> {
-//   const response = await fetch(input, init);
-//   if (!response.ok) {
-//     throw new Error(`Request failed: ${response.status}`);
-//   }
-//   return response;
+//
+// const rollDice2 = function (sides: number): number {
+//   return Math.floor(Math.random() * sides) + 1;
+// };
+//
+// const rollDice3 = (sides: number) => {
+//   return Math.floor(Math.random() * sides) + 1;
+// };
+//
+// function add(a: number, b: number) {
+//   return a + b;
 // }
+// function sub(a: number, b: number) {
+//   return a - b;
+// }
+// function mul(a: number, b: number) {
+//   return a * b;
+// }
+// function div(a: number, b: number) {
+//   return a / b;
+// }
+//
+// type BinaryFn = (a: number, b: number) => number;
+// const add2: BinaryFn = (a, b) => a + b;
+// const sub2: BinaryFn = (a, b) => a - b;
+// const mul2: BinaryFn = (a, b) => a * b;
+// const div2: BinaryFn = (a, b) => a / b;
+//
+// async function getQuote() {
+//   const response = await fetch('/quote?by=Mark+Twain');
+//   return await response.json();
+// }
+//
+// // async function checkedFetch(input: RequestInfo, init?: RequestInit) {
+// //   const response = await fetch(input, init);
+// //   if (!response.ok) {
+// //     // async関数で例外を投げると拒否されたPromiseになる
+// //     throw new Error(`Request failed: ${response.status}`);
+// //     return response;
+// //   }
+// // }
+//
+// // async function checkedFetch(
+// //   input: RequestInfo,
+// //   init?: RequestInit
+// // ): Promise<Response> {
+// //   const response = await fetch(input, init);
+// //   if (!response.ok) {
+// //     throw new Error(`Request failed: ${response.status}`);
+// //   }
+// //   return response;
+// // }
---
+++ src/item/20.ts
@@ -0,0 +1,36 @@
+interface Vector3D {
+  x: number;
+  y: number;
+  z: number;
+}
+function getComponent(vector: Vector3D, axis: 'x' | 'y' | 'z') {
+  return vector[axis];
+}
+
+const axis = 'x';
+// ^? const axis: "x"
+const vector: Vector3D = { x: 10, y: 20, z: 30 };
+getComponent(vector, axis as 'x' | 'y' | 'z');
+
+const obj2 = {
+  x: 1,
+};
+obj2.x = 3;
+// obj.x = '3';
+// obj.y = 4;
+// obj.z = 5;
+// obj.name = 'Pythagoras';
+
+const arr1 = [1, 2, 3];
+const arr2 = [1, 2, 3] as const;
+
+const frozenArray = Object.freeze([1, 2, 3]);
+// // ^? const frozenArray: readonly number[]
+const frozenObj = Object.freeze({ x: 1, y: 2 });
+
+type Point = [number, number];
+const capitals1 = { ny: [-73.7562, 42.6526], ca: [-121.4944, 38.5816] };
+const capitals2 = {
+  ny: [-73.7562, 42.6526],
+  ca: [-121.4944, 38.5816],
+} satisfies Record<string, Point>;
